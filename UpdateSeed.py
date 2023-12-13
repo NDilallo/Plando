dfe6a666492fed1b0ce33ebec85862166baed623
Nick Dilallo <nickd5465@gmail.com>
2023-12-12 21:51:33 -0600

    functionality for adding custom entrance

--- UpdateSeed.py
+++ UpdateSeed.py
@@ -8,11 +8,30 @@ def updateSeed(indexMaps: list) -> None:
         entrances = jsonData.get("entrances")
         
         for index1Enter, index1Exit, index2Enter, index2Exit in indexMaps:
+            found = False
             for entrance_dict in entrances:
                 if entrance_dict.get("index") == index1Enter:
                     entrance_dict["override"] = index2Enter
+                    found = True
                 if entrance_dict.get("index") == index2Exit:
                     entrance_dict["override"] = index1Exit
+            if not found:
+                custom_entrance = {
+                    "index": index1Enter,
+                    "destination": 936, #Doesnt matter?
+                    "blueWarp": 0,#Doesnt matter?
+                    "override": index2Enter,
+                    "overrideDestination": 578#Doesnt matter?
+                }
+                custom_exit = {
+                    "index": index2Exit,
+                    "destination": 936, #Doesnt matter?
+                    "blueWarp": 0,#Doesnt matter?
+                    "override": index1Exit,
+                    "overrideDestination": 578#Doesnt matter?
+                }
+                entrances.append(custom_entrance)
+                entrances.append(custom_exit)
         
         # Move to the beginning of the file to rewrite it with updated jsonData
         file.seek(0)
